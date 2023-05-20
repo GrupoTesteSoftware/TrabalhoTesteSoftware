52908d42adc1ad12b5f1c49a044f87a26c1c63a5
Matheus Schimieguel Silva <mschimieguel@gmail.com>
2023-05-20 19:13:10 -0300

    corrigindo teste CPF

--- test/test_validadores.py
+++ test/test_validadores.py
@@ -7,17 +7,17 @@ import Packages.validadores as validadores
 class TestValidarCPF(unittest.TestCase):
     def test_cpf_valido(self):
         cpf = '123.456.789-09'
-        resultado = validadores(cpf)
+        resultado = validadores.validarCPF(cpf)
         self.assertTrue(resultado)  # Espera-se que o resultado seja True
 
     def test_cpf_invalido(self):
         cpf = '123.456.789-10'
-        resultado = validadores(cpf)
+        resultado = validadores.validarCPF(cpf)
         self.assertFalse(resultado)  # Espera-se que o resultado seja False
 
     def test_cpf_formato_incorreto(self):
         cpf = '12345678910'  # Formato incorreto (sem pontos e traço)
-        resultado = validadores(cpf)
+        resultado = validadores.validarCPF(cpf)
         self.assertFalse(resultado)  # Espera-se que o resultado seja False
 
 if __name__ == '__main__':
