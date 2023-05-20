2547658ba365fac60f80316c9ffb87216d37bb65
Matheus Schimieguel Silva <mschimieguel@gmail.com>
2023-05-20 18:51:01 -0300

    Adicionando validadores

---
+++ packages/validadores.py
@@ -0,0 +1,11 @@
+def validarCPF(CPF:str) -> bool:
+    return True
+def validarCNPJ(CNPJ:str) -> bool:
+    return True
+def validarEmail(Email:str) -> bool:
+    return True
+def validarCelular(celular:str) -> bool:
+    return True
+def validarTelefoneFixo(telefoneFixo: str) -> bool:
+    return True
+
