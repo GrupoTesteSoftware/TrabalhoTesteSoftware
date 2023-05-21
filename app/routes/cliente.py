63817e9768a5e845e181e93ac555d99bacdee883
Matheus Schimieguel Silva <mschimieguel@gmail.com>
2023-05-21 00:47:05 -0300

    Removendo Codigo inutil

--- app/routes/cliente.py
+++ app/routes/cliente.py
@@ -35,69 +35,18 @@ def retrieve_empresa_clientes():
     return ClienteSchema(many=True).jsonify(result), 200
 
 
-@cliente.route('/cliente/pessoa', methods=['GET'])
+@cliente.route('/cliente/pessoaFisica', methods=['GET'])
 def retrieve_pessoa_clientes():
     result = models.Cliente.query.filter_by(tipo='Pessoa' ).all()
     return ClienteSchema(many=True).jsonify(result), 200
 
-@cliente.route('/cliente/pessoa/celular=<celular>', methods=['GET'])
-def retrieve_pessoa_celular_clientes(celular):
-    result = models.Cliente.query.filter_by(tipo='Pessoa' , celular=celular).all()
-    return ClienteSchema(many=True).jsonify(result), 200
-
-@cliente.route('/cliente/empresa/celular=<celular>', methods=['GET'])
-def retrieve_empresa_celular_clientes(celular):
-    result = models.Cliente.query.filter_by(tipo='Empresa' , celular=celular).all()
-    return ClienteSchema(many=True).jsonify(result), 200
 
 
 def js_to_py_datetime(str_datetime: str):
     str_datetime = str_datetime.replace('.000Z', '')
     return datetime.strptime(str_datetime, '%Y-%m-%d')
 
-@cliente.route('/cliente/', methods=['POST'])
-def create_cliente():
-    """Create post cliente"""
-    response_data = json.loads(request.data.decode())
 
-    id = response_data['id']
-    nome = response_data['nome']
-    email = response_data['email']
-    telefone = response_data['telefone']
-    tipo = response_data['tipo']
-    celular = response_data['celular']
-    data = js_to_py_datetime(response_data['data'])
-    dataNascimento = js_to_py_datetime(response_data['dataNascimento'])
-
-    if int(id) == -1:
-        cliente_obj = models.Cliente(
-            nome = nome,
-            email = email,
-            telefone = telefone,
-            tipo = tipo,
-            celular = celular,
-            data = data,
-            dataNascimento = dataNascimento
-        )
-
-        db.session.add(cliente_obj)
-    else:
-        cliente_obj = models.Cliente.query.filter_by(id=id).first()
-
-        setattr(cliente_obj, 'nome', nome)
-        setattr(cliente_obj, 'email', email)
-        setattr(cliente_obj, 'telefone', telefone)
-        setattr(cliente_obj, 'tipo', tipo)
-        setattr(cliente_obj, 'celular', int(celular))
-        setattr(cliente_obj, 'data', data)
-        setattr(cliente_obj, 'dataNascimento', dataNascimento)
-
-    db.session.commit()
-    response = {
-        'success': True,
-    }
-    status_code = 200
-    return jsonify(response), status_code
 
 @cliente.route('/cliente/<id>/atualizar', methods=['PATCH'])
 def atualizar_Cadastro_Cliente(id):
@@ -186,76 +135,12 @@ def cadastrar_cliente():
 
 
 
-@cliente.route('/cliente/update/', methods=['POST'])
-def update_cliente():
-    """Update post cliente"""
-    id = request.form['id']
-    nome = request.form['nome']
-    email = request.form['email']
-    telefone = request.form['telefone']
-    tipo = request.form['tipo']
-    celular = request.form['celular']
-    data = request.form['data']
-    dataNascimento = request.form['dataNascimento']
-
-    cliente_obj = models.Cliente.query.filter_by(id=id).first()
-
-    setattr(cliente_obj, 'nome', nome)
-    setattr(cliente_obj, 'email', email)
-    setattr(cliente_obj, 'telefone', telefone)
-    setattr(cliente_obj, 'tipo', tipo)
-    setattr(cliente_obj, 'celular', int(celular))
-    setattr(cliente_obj, 'data', data)
-    setattr(cliente_obj, 'dataNascimento', dataNascimento)
-
-    db.session.commit()
-    response = {
-        'success': True,
-    }
-    status_code = 200
-    return jsonify(response), status_code
-
 @cliente.route('/cliente/update_column/', methods=['POST'])
 def update_cliente_column():
     """Update post cliente column"""
     id = request.form['id']
     key = request.form['key']
     value = request.form['value']
-
-    cliente_obj = models.Cliente.query.filter_by(id=id).first()
-
-    if key == "celular":
-        value = int(value)
-
-    setattr(cliente_obj, key, value)
-
-    db.session.commit()
-    response = {
-        'success': True,
-    }
-    status_code = 200
-    return jsonify(response), status_code
-
-
-
-@cliente.route('/cliente/<nome>', methods=['GET'])
-def create_get_cliente(nome):
-    """Create get cliente"""
-    cliente_obj = models.Cliente( 
-        nome=nome
-    )
-
-    db.session.add(cliente_obj)
-    db.session.commit()
-    response = {
-        'success': True,
-    }
-    status_code = 200
-    return jsonify(response), status_code
-
-@cliente.route('/cliente/update/<id>/<key>/<value>', methods=['GET'])
-def update_get_cliente_column(id, key, value):
-    """Update get cliente column"""
 
     cliente_obj = models.Cliente.query.filter_by(id=id).first()
 
