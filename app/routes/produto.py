0e187b49dac3b45d18da742fae93d9d8e213f7e0
Matheus Schimieguel Silva <mschimieguel@gmail.com>
2023-05-21 21:28:42 -0300

    alterando rota cadastrar Produto

--- app/routes/produto.py
+++ app/routes/produto.py
@@ -5,37 +5,37 @@ from app.serializers import ProdutoSchema
 from flask import Blueprint, jsonify, request
 from json import dumps as jsondump
 import app.response as response
-
+import json
 Produto = Blueprint('Produto', __name__)
 
 # Parameters: https://stackoverflow.com/questions/28229668/python-flask-how-to-get-route-id-from-url
 
 @Produto.route('/Produtos/', methods=['GET'])
-def retrieve_all_Produtos():
-    """Retrieve Produto"""
-    '''
-    id  = request.args.get('id', None)
-    contact  = request.args.get('contact', None)
-    company  = request.args.get('company', None)
+def listar_todos_produtos():
 
-    if not id and not contact and not company:
-        response = models.Produto.query.all()
-    elif not contact and not company:
-        response = models.Produto.query.filter_by(id=id).first()
-    '''
     result = models.Produto.query.all()
     return ProdutoSchema(many=True).jsonify(result), 200
 
-@Produto.route('/Produto/', methods=['PUT'])
-def cadasttar_Produto():
-    """Create post Produto"""
-    contact = request.form['contact']
-    company = request.form['company']
-    Produto_obj = models.Produto(
-        contact=contact, 
-        company=company
-    )
+@Produto.route('/Produto/cadastrar', methods=['PUT'])
+def cadastrar_produto():
+    codigoBarras = None
+    response_data = json.loads(request.data.decode())
 
+    if 'nome' not in response_data:
+        return response.bad_request("Para cadastrar um produto é preciso de um nome para ele")
+    if 'idFornecedor' not in response_data:
+        return response.bad_request("Para cadastrar um produto atrelar aum forneceedor")
+    
+    if  'codigoBarras' in response_data:    
+        codigoBarras = response_data['codigoBarras']
+    idFornecedor = response_data['idFornecedor']
+    nome = response_data['nome']
+    Produto_obj = models.Produto(
+        nome=nome, 
+        codigoBarras=codigoBarras,
+        idFornecedor=idFornecedor
+
+    )
     db.session.add(Produto_obj)
     db.session.commit()
 
